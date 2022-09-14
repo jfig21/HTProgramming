de82023983972c3dcd7bbfa921e4646f37397ae2
Quiterio <jfig@ipg.pt>
2022-09-14 11:48:55 +0100

    Update DadosPeduca.java

--- service/DadosPeduca.java
+++ service/DadosPeduca.java
@@ -25,8 +25,8 @@ public interface DadosPeduca {
  // Acesso local / IPG   
 //    String [] tHost = new String[]{
 //                "localhost",        // iHost = 0
-//                "192.168.23.32",    // iHost = 1 Rede IPG: bd.ipg.pt
-//                "bd.ipg.pt"};//"172.16.18.89" };//193.137.162.19"};  // iHost = 2 RedeExterna
+//                "192.0.0.0",    // iHost = 1 Rede IPG: bd.ipg.pt
+//                "193.0.0.0"};   // iHost = 2 RedeExterna
 //    String defaultBD = "";
 //    String user = "";
 //    String passwd = "";
